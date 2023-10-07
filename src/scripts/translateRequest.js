e1429d71f9e40e6bcefa59b051d018fc97628163
IllyaHavrulyk <illyahavrulyk@gmail.com>
2023-10-07 19:38:39 +0300

    Fixed issue with sending request on backend.

--- src/scripts/translateRequest.js
+++ src/scripts/translateRequest.js
@@ -3,13 +3,10 @@ import axios from "axios";
 export async function getTranslation(providedText, targetLanguage) {
   let translatedText = await axios({
     method: "post",
-    url: "https://api-translate.systran.net/translation/text/translate",
+    url: "http://localhost:8080/translations/translate",
     responseType: "json",
-    headers: {
-      Authorization: "Key c9fe10f4-1dba-4d97-adb9-5b15b7d660c2",
-    },
     data: {
-      input: providedText,
+      input: [providedText],
       target: targetLanguage,
     },
   });
